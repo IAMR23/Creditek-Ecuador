aebbf2082d96ce2d3fc0a625806c9130a165e4ad
IAMR23 <alexanderpilataxi232002@gmail.com>
2025-12-08 14:28:47 -0500

    Imagenes uploads docker

--- frontend/src/components/PublicRoute.jsx
+++ frontend/src/components/PublicRoute.jsx
@@ -5,6 +5,7 @@ export default function PublicRoute({ children, isAuthenticated, rol }) {
     // Si ya está logueado, redirigir según el rol
     if (rol === "admin") return <Navigate to="/dashboard" replace />;
     if (rol === "Vendedor") return <Navigate to="/vendedor-panel" replace />;
+    if (rol === "Repartidor") return <Navigate to="/logistica-panel" replace />;
   }
 
   // Si NO está autenticado → mostrar el contenido (login)
