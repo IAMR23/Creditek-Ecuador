47a673cf41761d1f1de29ebd7f59c290392b5172
IAMR23 <alexanderpilataxi232002@gmail.com>
2026-01-05 00:28:02 -0500

    SEO

--- landingpage/src/const/constSocialNetworks.ts
+++ landingpage/src/const/constSocialNetworks.ts
@@ -1,2 +1,2 @@
-  export const FBURL = "https://www.facebook.com/dralexandrafierro/"
-  export const INS = "https://www.instagram.com/draalexandrafierro/"
+  export const FBURL = "https://www.facebook.com/creditekmatriz"
+  export const INS = "https://www.instagram.com/creditek24?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
